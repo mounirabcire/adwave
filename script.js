9b485c57b81953b27cb4c4c2bac14e71499a0288
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-11-08 15:50:46 +0100

    updated

--- script.js
+++ script.js
@@ -10,22 +10,57 @@ const h1Span = document.querySelector('.header__animated');
 const companyName = 'Adwave.'.split('');
 let currentCompanyNameIndex = 0;
 
-h1Span.textContent = '';
-
 function animateCompanyName() {
+    if (!h1Span) return;  
+
     if (currentCompanyNameIndex < companyName.length) {
         h1Span.textContent += companyName[currentCompanyNameIndex];
         currentCompanyNameIndex++;
         setTimeout(animateCompanyName, 500);
     } else {
-        // Reset the index and clear the text after the full name is displayed
         currentCompanyNameIndex = 0;
         setTimeout(() => {
             h1Span.textContent = '';
             animateCompanyName();
-        }, 1000); // Delay before restarting the animation
+        }, 1000);
     }
 }
 
-// Start the animation
-animateCompanyName();
+if (h1Span) {
+    h1Span.textContent = '';
+    animateCompanyName();
+}
+
+// Slider
+const tracks = document.querySelectorAll('.slider-track');
+console.log(1);
+
+tracks.forEach((track) => {
+    track.addEventListener('mousedown', (e) => {
+        track.dataset.mouseDownAt = e.clientX;
+        track.dataset.prevPercentage = track.dataset.percentage || 0;
+        track.classList.add('dragging');
+    });
+
+    track.addEventListener('mouseup', () => {
+        track.dataset.mouseDownAt = 0;
+        track.dataset.prevPercentage = track.dataset.percentage;
+        track.classList.remove('dragging');
+    });
+
+    track.addEventListener('mousemove', (e) => {
+        if (track.dataset.mouseDownAt == 0) return;
+
+        const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+        const maxDelta = window.innerWidth / 2;
+        const percentage = (mouseDelta / maxDelta) * -100;
+        const nextPercentageUnconstrained =
+            parseFloat(track.dataset.prevPercentage) + percentage;
+        const nextPercentage = Math.max(
+            Math.min(nextPercentageUnconstrained, 0),
+            -100
+        );
+        track.dataset.percentage = nextPercentage;
+        track.style.transform = `translateX(${nextPercentage}%)`;
+    });
+});
